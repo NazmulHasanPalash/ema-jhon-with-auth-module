3bb2b8e98302ec0c24251ece508698f87a50c418
polash <an>
2021-10-27 20:33:17 +0600

    woork done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,9 @@
-console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyDH-pKy94te7kBF8qRmk0EmRJj5XPBFnhA",
+    authDomain: "ema-jhon-with-auth-428f7.firebaseapp.com",
+    projectId: "ema-jhon-with-auth-428f7",
+    storageBucket: "ema-jhon-with-auth-428f7.appspot.com",
+    messagingSenderId: "454996810151",
+    appId: "1:454996810151:web:5587a7560b8e117590be82"
 };
-
 export default firebaseConfig;
